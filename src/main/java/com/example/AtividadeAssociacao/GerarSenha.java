406f18946156524fab3af9c3333c7bcee1672319
Marcos <marcosviniciusrdca2@gmail.com>
2025-12-16 21:04:24 -0300

    ajustado os inserts do data.sql. foi retirado o CommandLineRunner da main que estava impedindo a inserção dos dados do data.sql

---
+++ src/main/java/com/example/AtividadeAssociacao/GerarSenha.java
@@ -0,0 +1,11 @@
+package com.example.AtividadeAssociacao;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class GerarSenha {
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("admin123"));
+    }
+}
